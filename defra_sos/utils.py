7bb684c7df75ecc472cda008a77607eff6258b10
Joe Heffer <j.heffer@sheffield.ac.uk>
2020-03-30 12:14:18 +0100

    Tidied up code

--- defra_sos/utils.py
+++ defra_sos/utils.py
@@ -4,8 +4,10 @@ Random utility functions
 
 import os
 import argparse
-from datetime import datetime
+import datetime
 import json
+
+import constants
 
 
 def build_dir(path):
@@ -21,12 +23,17 @@ def build_output_dir_for_meta(root_output_dir, folder_name):
     return output_dir
 
 
-def build_output_dir_for_data(root_output_dir, date: str) -> str:
+def build_output_dir_for_data(root_output_dir, date_stamp: str) -> str:
     """Prepare output directory for each date"""
 
-    output_dir = os.path.join(root_output_dir, *date.split('-'))
+    output_dir = os.path.join(root_output_dir, *date_stamp.split('-'))
     os.makedirs(output_dir, exist_ok=True)
     return output_dir
+
+
+def date(timestamp: str) -> datetime.date:
+    t = datetime.datetime.strptime(timestamp, constants.DATE_FORMAT)
+    return t.date()
 
 
 def get_args(description, args=None) -> argparse.Namespace:
@@ -34,13 +41,11 @@ def get_args(description, args=None) -> argparse.Namespace:
 
     parser = argparse.ArgumentParser(description=description)
 
-    parser.add_argument('-d', '--date', required=True, type=lambda s: datetime.strptime(s, '%Y-%m-%d'),
-                        help="ISO UTC date")
+    parser.add_argument('-d', '--date', required=True, type=date, help="ISO UTC date")
     parser.add_argument('-od', '--output_data', required=True, type=str, help="Output CSV file path")
     parser.add_argument('-k', '--distance', type=int, help="Radius distance (km)", default=30)
     parser.add_argument('-um', '--update_meta', action='store_true', help="True if update the metadata")
-    parser.add_argument('-om', '--output_meta', type=str, help="Output folder path for metadata files",
-                        default="meta")
+    parser.add_argument('-om', '--output_meta', type=str, help="Output folder path for metadata files", default="meta")
     parser.add_argument('-v', '--verbose', action='store_true', help='Show debug logs')
     parser.add_argument('-ad', '--assets-dir', type=str, help="Assets directory", default="assets")
 
@@ -55,4 +60,3 @@ def get_value1(obj: json, path: str):
     for i in range(len(p)):
         result = result.get(p[i], '')
     return result if type(result) != tuple else result[0]
-
