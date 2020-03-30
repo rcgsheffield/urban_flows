3f9af1d84e3c192aa1fec9cd94f30d438fe67d5d
Joe Heffer <j.heffer@sheffield.ac.uk>
2020-03-30 13:23:12 +0100

    Removed get_values test

--- defra_sos/tests/tests_utils.py
+++ defra_sos/tests/tests_utils.py
@@ -7,14 +7,8 @@ import unittest
 import os
 import argparse
 import datetime
-import json
 
 import utils
-import assets
-import download
-import http_session
-import metadata
-import pipeline
 
 LOGGER = logging.getLogger(__name__)
 
@@ -29,8 +23,8 @@ class TestUtils(unittest.TestCase):
         result = expected if os.path.exists("{}/{}".format(param1, param2)) else "The dir does not exists"
         os.rmdir("{}/{}".format(param1, param2))
         os.rmdir(param1)
-        self.assertEqual(result, expected, "Should be '{}'".format(expected))
 
+        self.assertEqual(result, expected, "Should be '{}'".format(expected))
 
     def test_build_output_dir_for_meta(self):
         param1 = "param1"
@@ -40,36 +34,29 @@ class TestUtils(unittest.TestCase):
         result = expected if os.path.exists("{}/{}".format(param1, param2)) else "The dir does not exists"
         os.rmdir("{}/{}".format(param1, param2))
         os.rmdir(param1)
-        self.assertEqual(result, expected, "Should be '{}'".format(expected))
 
+        self.assertEqual(result, expected, "Should be '{}'".format(expected))
 
     def test_build_output_dir_for_data(self):
         param1 = "param1"
         param2 = "param2"
         utils.build_output_dir_for_data(param1, param2)
         expected = "Output dir for meta is build"
-        result = expected if os.path.exists("{}/{}".format(param1,param2)) else "The dir does not exists"
+        result = expected if os.path.exists("{}/{}".format(param1, param2)) else "The dir does not exists"
         os.rmdir("{}/{}".format(param1, param2))
         os.rmdir(param1)
-        self.assertEqual(result, expected, "Should be '{}'".format(expected))
 
+        self.assertEqual(result, expected, "Should be '{}'".format(expected))
 
     def test_get_args(self):
         param1 = "description"
         args = "-d 2020-03-10 -od flood.csv -k 25 -um True -om meta -v True -ad assets".split(" ")
-        result = utils.get_args(param1,args)
+        result = utils.get_args(param1, args)
         expected = argparse.Namespace(assets_dir='assets', date=datetime.datetime(2020, 3, 10, 0, 0),
-                                    distance=25, output_data='flood.csv', output_meta='meta', update_meta=True,
-                                    verbose=True)
+                                      distance=25, output_data='flood.csv', output_meta='meta', update_meta=True,
+                                      verbose=True)
+
         self.assertEqual(result, expected, "The returned namespace should be correct'".format(expected))
-
-
-    def test_get_value(self):
-        param1 = json.loads('{"param1":"value1"}')
-        param2 = "param1"
-        result = utils.get_value(param1, param2)
-        expected = "value1"
-        self.assertEqual(result, expected, "The returned value should be '{}'".format(expected))
 
 
 if __name__ == '__main__':
