fb6a88097740d1503808b2bd8759239af4829537
Joe Heffer <j.heffer@sheffield.ac.uk>
2020-05-14 11:56:20 +0100

    Convert TTN device to UF assets

---
+++ the_things_stack/README.md
@@ -0,0 +1,20 @@
+# The Things Stack
+
+This Python module gets information about an application on [The Things Network](https://www.thethingsnetwork.org/) and retrieves the necessary metadata for use in [Urban Flows Observatory](https://urbanflows.ac.uk/).
+
+It uses the Software Development Kit (SDK) provided the The Things Stack gRPC APIs, specifically the [Application Manager API](https://www.thethingsnetwork.org/docs/applications/manager/). See docs: [Build applications on The Things Network](https://www.thethingsnetwork.org/docs/applications/).
+
+# Installation
+
+```bash
+$ pip install -r requirements.txt
+```
+
+
+
+# Usage
+
+```bash
+$ python uftts --help
+```
+
--- the_things_stack/uftts/__main__.py
+++ the_things_stack/uftts/__main__.py
@@ -3,7 +3,15 @@ import argparse
 
 import ttn
 
+from ufmetadata.assets import Site, Sensor
+
+USAGE = """
+python uftts --app_id <my_app_id> --access_key <my_token>
+"""
+
 DESCRIPTION = """
+The Things Network
+
 The Things Stack
 
 https://www.thethingsnetwork.org/docs/applications/
@@ -15,13 +23,40 @@ LOGGER = logging.getLogger(__name__)
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser = argparse.ArgumentParser(usage=USAGE, description=DESCRIPTION)
 
     parser.add_argument('-a', '--app_id', required=True, help='Application identifier')
     parser.add_argument('-t', '--access_key', help='Authentication token')
     parser.add_argument('-v', '--verbose', action='store_true', help='Logging debug level')
 
     return parser.parse_args()
+
+
+def build_device_url(app_id, dev_id):
+    return "https://console.thethingsnetwork.org/applications/{app_id}/devices/{dev_id}".format(
+        app_id=app_id,
+        dev_id=dev_id,
+    )
+
+
+def device_to_site(device, app_id) -> Site:
+    return Site(
+        site_id=device.dev_id,
+        latitude=device.latitude,
+        longitude=device.longitude,
+        altitude=device.altitude,
+        country='United Kingdom',
+        desc_url=build_device_url(app_id=app_id, dev_id=device.dev_id),
+    )
+
+
+def device_to_sensor(device, app_id) -> Sensor:
+    return Sensor(
+        sensor_id=device.dev_id,
+        family="The Things Network {}".format(app_id),
+        detectors=list(),
+        desc_url=build_device_url(app_id=app_id, dev_id=device.dev_id),
+    )
 
 
 def main():
@@ -35,7 +70,10 @@ def main():
     # List Devices
     # https://github.com/TheThingsNetwork/api/blob/master/handler/handler.proto#L91
     for device in app.devices():
-        print(device.dev_id, device.latitude, device.longitude, sep='\t')
+        LOGGER.info("Device '%s'", device.dev_id)
+
+        print(device_to_site(device, app_id=args.app_id))
+        print(device_to_sensor(device, app_id=args.app_id))
 
 
 if __name__ == '__main__':
