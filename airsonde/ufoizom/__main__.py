4789f5925bd2fa686a4978e1723afe75bc863769
Joe Heffer <j.heffer@sheffield.ac.uk>
2020-07-14 11:59:00 +0100

    Rename metrics for CSV output

--- airsonde/ufoizom/__main__.py
+++ airsonde/ufoizom/__main__.py
@@ -1,6 +1,10 @@
 import argparse
 import logging
 import datetime
+import csv
+import pathlib
+
+from collections import OrderedDict
 
 import ufoizom.http_session
 import ufoizom.utils
@@ -20,6 +24,11 @@ python . TODO
 LOGGER = logging.getLogger(__name__)
 
 
+class UrbanDialect(csv.excel):
+    """CSV format options"""
+    delimiter = '|'
+
+
 def parse_date(date: str) -> datetime.date:
     return datetime.datetime.strptime(date, ufoizom.settings.DATE_FORMAT).date()
 
@@ -28,8 +37,31 @@ def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(usage=USAGE, description=DESCRIPTION)
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-c', '--config', help='Configuration file', default=ufoizom.settings.DEFAULT_CONFIG_FILE)
-    parser.add_argument('-d', '--date', type=parse_date, required=True)
+    parser.add_argument('-d', '--date', type=parse_date, required=True, help='YYYY-MM-DD')
+    parser.add_argument('-o', '--output', help='Path of output file', required=True, type=pathlib.Path)
     return parser.parse_args()
+
+
+def transform(row: dict) -> dict:
+    """Clean a row of data"""
+    # Rename metrics
+    row = {new: row[old] for old, new in ufoizom.settings.METRICS.items()}
+
+    # Parse Unix timestamp
+    row['timestamp'] = datetime.datetime.fromtimestamp(row['timestamp'], tz=datetime.timezone.utc).isoformat()
+
+    # Preserve column order
+    return OrderedDict(row)
+
+
+def write_csv(path: pathlib.Path, rows):
+    headers = tuple(ufoizom.settings.METRICS.values())
+    LOGGER.info('CSV headers %s', headers)
+    with path.open('w') as file:
+        writer = csv.DictWriter(file, fieldnames=headers, dialect=UrbanDialect)
+        writer.writerows(rows)
+
+        LOGGER.info("Wrote '%s'", file.name)
 
 
 def main():
@@ -42,8 +74,16 @@ def main():
     end = datetime.datetime.combine(args.date + datetime.timedelta(days=1), datetime.time.min)
     average = datetime.timedelta(seconds=60)
 
+    current = Data.current(session)
+    row = current.pop('payload').pop('d')
+    LOGGER.info(current)
+    row = transform(row)
+
+    write_csv(args.output, rows=[row])
+
+    exit()
     for device in Device.list(session):
-        print(device)
+        LOGGER.info("DEVICE %s", device)
         for data in Data.analytics(session, device['deviceId'], start, end, average):
             print(data)
 
--- airsonde/ufoizom/settings.py
+++ airsonde/ufoizom/settings.py
@@ -1,14 +1,69 @@
 import pathlib
 
+from collections import OrderedDict
+
 DATE_FORMAT = '%Y-%m-%d'
 DEFAULT_CONFIG_FILE = str(pathlib.Path.home().joinpath('oizom.cfg'))
 FAMILY = 'EMS_AirSonde'
 DESC_URL = 'https://terminal.oizom.com/#/u/devices/info'
 
-METRICS = dict(
-    # uv = ultraviolet index
-    # light = lux
-    temp='MET_TEMP',
-    hum='MET_RH',
-    bs='INST_BATTERY_PERCENT',  # battery strength
+# Map data labels to the Urban Flows metric names
+# See table in docs/Polludrone SMART Parameters Table.pdf
+# TODO check new metric names are correct
+# This dictionary determines the order of the columns in the output data
+METRICS = OrderedDict((
+    ('t', 'timestamp'),
+    ('bs', 'INST_BATTERY_PERCENT'),  # battery level
+    ('g1', 'AQ_CO2'),
+    ('g2', 'AQ_CO'),
+    ('g3', 'AQ_NO2'),
+    # ('g4', 'AQ_NH3'),
+    ('g5', 'AQ_O3'),
+    # ('g6', 'AQ_H2S'),
+    ('g7', 'AQ_NO'),
+    ('g8', 'AQ_SO2'),
+    # ('g9', 'AQ_O2'),
+    ('p1', 'AQ_PM25'),
+    ('p2', 'AQ_PM10'),
+    #('p3', 'AQ_PM1'),
+    ('temp', 'MET_TEMP'),  # Celsius
+    ('hum', 'MET_RH'),  # Humidity (%)
+    ('leq', 'AQ_NOISE'),  # Noise average (dB)
+    ('lmin', 'AQ_NOISE/MIN'),  # Noise minimum (dB)
+    ('lmax', 'AQ_NOISE/PEAK'),  # Noise maximum (dB)
+    #('rain', 'MET_RAIN'),  # TODO rainfall (mm)
+    ('light', 'MET_LIGHT'),  # TODO Visible light (lux)
+    #('flood', 'MET_WATER'),  # TODO Flood level (mm)
+    ('uv', 'MET_UV'),  # TODO ultraviolet index
+    #('wd', 'MET_WIND_DIRECTION'),  # TODO Wind direction (degrees)
+    # ('ws', 'MET_WIND_SPEED'),  # TODO wind speed (m/s)
+    # ('pr', 'MET_AP'),  # Atmospheric Pressure (hPa)
+))
+
+UNITS = dict(
+    bs='%',
+    g1='ppm',
+    g2='mg/m3',
+    g3='ug/m3',
+    g4='ug/m3',
+    g5='ug/m3',
+    g6='ug/m3',
+    g7='ug/m3',
+    g8='ug/m3',
+    g9='%',
+    p1='ug/m3',
+    p2='ug/m3',
+    p3='ug/m3',
+    temp='C',
+    hum='%',
+    leq='dB',
+    lmin='dB',
+    lmax='dB',
+    rain='mm',
+    light='lx',
+    flood='mm',
+    uv='',  # units of one
+    ws='m/s',
+    wd='deg',
+    pr='hPA',
 )
