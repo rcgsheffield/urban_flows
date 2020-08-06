135cf9b5c168e9529e2a074beec46589b7fb581c
Joe Heffer <j.heffer@sheffield.ac.uk>
2020-08-06 11:33:34 +0100

    Fix path

--- sheffsense/install.sh
+++ sheffsense/install.sh
@@ -1,3 +1,3 @@
 cp nginx/sites-available/* nginx/sites-available --verbose
-ln -s /etc/nginx/sites-available/sheffsense.uk /etc/nginx/sites-enabled/sheffsense.uk
-cp pm2/pm2-sheffsense.service /etc/systemd/service/pm2-sheffsense.service
+ln -s /etc/nginx/sites-available/sheffsense.uk /etc/nginx/sites-enabled/sheffsense.uk --verbose
+cp pm2/pm2-sheffsense.service /etc/systemd/system/pm2-sheffsense.service --verbose
