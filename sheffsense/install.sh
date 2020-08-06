715fe9055d0a874d9bbfb45aadca8888f3c978db
Joe Heffer <j.heffer@sheffield.ac.uk>
2020-08-06 11:34:34 +0100

    force overwrite symbolic link

--- sheffsense/install.sh
+++ sheffsense/install.sh
@@ -1,3 +1,3 @@
-cp nginx/sites-available/* nginx/sites-available --verbose
-ln -s /etc/nginx/sites-available/sheffsense.uk /etc/nginx/sites-enabled/sheffsense.uk --verbose
+cp nginx/sites-available/* /etc/nginx/sites-available --verbose
+ln -s /etc/nginx/sites-available/sheffsense.uk /etc/nginx/sites-enabled/sheffsense.uk --verbose --force
 cp pm2/pm2-sheffsense.service /etc/systemd/system/pm2-sheffsense.service --verbose
