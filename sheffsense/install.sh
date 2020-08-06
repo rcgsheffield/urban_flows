b202c1f19db961f0793a7f43b9e9c2df65e65948
Joe Heffer <j.heffer@sheffield.ac.uk>
2020-08-06 11:43:50 +0100

    Add software install line

--- sheffsense/install.sh
+++ sheffsense/install.sh
@@ -1,3 +1,4 @@
 cp nginx/sites-available/* /etc/nginx/sites-available --verbose
 ln -s /etc/nginx/sites-available/sheffsense.uk /etc/nginx/sites-enabled/sheffsense.uk --verbose --force
 cp pm2/pm2-sheffsense.service /etc/systemd/system/pm2-sheffsense.service --verbose
+cp ~/ClimateMonitorV2/ClimateMonitorV2 /opt/sheffsense
