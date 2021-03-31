48994b00b943c2a8ba723f15e308af6ba72563d1
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-03-31 09:09:47 +0100

    improve installation script

--- awesome/install.sh
+++ awesome/install.sh
@@ -1,5 +1,13 @@
+#!/usr/bin/env sh
+
+TARGET_DIR=/opt/awesome
+
 echo "Installing production settings..."
 cp --verbose settings_prod.py settings_local.py
+
+echo "Installing data bridge into $TARGET_DIR..."
+mkdir -v $TARGET_DIR
+sudo cp -rv * $TARGET_DIR
 
 echo "Installing systemd units..."
 cp --verbose databridge.service /etc/systemd/system/databridge.service
