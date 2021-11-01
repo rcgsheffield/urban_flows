074346a3fb8932316a80bdf8ff4939c7ec14f3fa
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-11-01 13:22:18 +0000

    fix install dirs

--- data_logger_server/install.sh
+++ data_logger_server/install.sh
@@ -30,7 +30,8 @@ chmod 770 /run/dlsrv
 
 # Install application files
 mkdir --parents --verbose $DEST_DIR
-cp --recursive ./* $DEST_DIR
+cp --recursive ./data_logger_server $DEST_DIR
+cp --recursive ./wsgi $DEST_DIR/wsgi
 
 # Create log directory
 mkdir -p $LOG_DIR
