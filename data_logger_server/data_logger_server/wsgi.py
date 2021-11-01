d81936f75f1b49ef6f7a395cfb1d750586aa8201
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-11-01 15:05:04 +0000

    .

--- data_logger_server/data_logger_server/wsgi.py
+++ data_logger_server/data_logger_server/wsgi.py
@@ -1,10 +1,8 @@
 """
 WSGI entry point
+https://flask.palletsprojects.com/en/2.0.x/deploying/wsgi-standalone/#uwsgi
 """
 
 import app_factory
 
 app = app_factory.create_app()
-
-if __name__ == '__main__':
-    app.run()
